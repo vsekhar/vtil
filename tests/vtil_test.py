ae4cf089166d5922c51bc07f0c304b941ebf50fb
Vivek Sekhar <vivek@viveksekhar.ca>
2012-02-12 12:45:05 -0800

    Nose-compliant tests

---
+++ tests/vtil_test.py
@@ -0,0 +1,51 @@
+'''
+Created on Jan 14, 2012
+
+@author: vsekhar
+'''
+
+import unittest
+import collections
+from types import NotImplementedType
+
+import vtil
+from vtil import random
+
+class UtilTest(unittest.TestCase):
+    def test_fixed_int(self):
+        s = '0000000021415'
+        self.assertEqual(s, vtil.fixed_int(int(s), len(s)))
+
+class PartitionTest(unittest.TestCase):
+    def test_partition(self):
+        p = vtil.Partitioner(6)
+        self.assertEqual(6, len(p))
+        self.assertRaises(NotImplementedError, p, 'abc123')
+
+    def isUniform(self, partitioner, delta=0.2, rfunc=None):
+        def def_rfunc(): return randomtools.random_string(8)
+        rfunc = rfunc or def_rfunc
+        samples = 10000
+        buckets = len(partitioner)
+
+        expected_count = float(samples) / buckets
+
+        c = collections.Counter((partitioner(rfunc()) for _ in xrange(samples)))
+        for _, count in c.most_common():
+            self.assertAlmostEqual(expected_count, count, delta=expected_count*delta)
+
+    def test_hash_partitioner(self):
+        h = vtil.HashPartitioner(6)
+        self.assertEqual(len(h), 6)
+        self.isUniform(h, 0.1)
+
+    def test_string_partitioner(self):
+        h = vtil.StringPartitioner(6)
+        self.assertEqual(len(h), 6)
+        self.isUniform(h, 0.2)
+    
+    def test_number_partitioner(self):
+        h = vtil.NumberPartitioner(0, 1, 6)
+        self.assertEqual(len(h), 6)
+        self.isUniform(h, 0.1, rfunc = randomtools.random)
+
