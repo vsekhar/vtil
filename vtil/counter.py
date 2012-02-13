443bd994e40c644d1922ca83711c651238245afa
Vivek Sekhar <vivek@viveksekhar.ca>
2012-02-13 11:45:53 -0800

    Fake a counter for pre 2.7

---
+++ vtil/counter.py
@@ -0,0 +1,16 @@
+try:
+    from collections import Counter # only in 2.7
+except ImportError:
+    # imitate some basic functionality for <2.7
+    from operator import itemgetter
+    from collections import defaultdict
+    class Counter(defaultdict):
+        def __init__(self, iterable):
+            super(defaultdict, self).__init__()
+            self._dict = defaultdict(lambda: 0)
+            for item in iterable:
+                self._dict[item] += 1
+
+        def most_common(self):
+            for k,v in sorted(self._dict.iteritems(), key=itemgetter(1)):
+                yield k,v
