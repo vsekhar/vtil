854193ad1eb04c23a683caedc84ef14643e1e20a
Vivek Sekhar <vivek@viveksekhar.ca>
2012-02-13 00:05:00 -0800

    comments

--- vtil/chunk.py
+++ vtil/chunk.py
@@ -4,7 +4,7 @@ import sys
 from vtil import accum
 
 def chunks(n, iterable):
-	"chunkify(4, [1,2,3,4,5,6]) --> 1234 56"
+	"chunks(4, [1,2,3,4,5,6]) --> 1234 56"
 	itr = iter(iterable)
 	while True:
 		ret = tuple(itertools.islice(itr, n))
@@ -12,11 +12,11 @@ def chunks(n, iterable):
 		else: break
 
 def padded_chunks(n, iterable, fillvalue=None):
-	"chunkify_pad(4, [1,2,3,4,5,6], '-') --> 1234 56--"
+	"padded_chunks(4, [1,2,3,4,5,6], '-') --> 1234 56--"
 	return itertools.izip_longest(fillvalue=fillvalue, *[iter(iterable)]*n)
 
 def full_chunks_only(n, iterable):
-	"chunkify_drop(4, [1,2,3,4,5,6]) --> 1234"
+	"full_chunks_only(4, [1,2,3,4,5,6]) --> 1234"
 	return itertools.izip(*[iter(iterable)]*n)
 
 def mem_chunks(iterable, max_mem=None):
